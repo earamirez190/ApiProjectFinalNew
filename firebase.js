b0e0a904636a03d2c688479fcfd2bc2d18d8035e
Edward Ramirez <e.ramirez190@gmail.com>
2024-05-21 19:06:12 -0500

    modifica firebase.js y variable .env FIREBASE_SERVICE_ACCOUNT

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+/*
 require("dotenv").config()
 
 const{initializeApp, applicationDefault}=require("firebase-admin/app")
@@ -10,3 +11,16 @@ const db = getFirestore()
 module.exports= {
     db
 }
+*/
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://proyectfinal-b86ae-default-rtdb.firebaseio.com/'
+});
+
+const db = admin.firestore();
+
+module.exports = { db };
